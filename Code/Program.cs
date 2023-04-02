901b8e3502da68d53945fac29136663d6c71afe8
Karina Radzevich <karina_radzevich@bk.ru>
2023-04-03 01:37:40 +0400

    method fill array is done

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int lenght = 4;
+string[] array = new string[lenght];
+Console.WriteLine("Введите 4 элемента первоначального массива.Каждый элемент вводится через ENTER: ");
+array = FillArray(array);
+// Метод заполнения массива от пользователя
+string[] FillArray(string[] someArray)
+{
+    for (int i = 0; i < someArray.Length; i++)
+    {
+        someArray[i] = Console.ReadLine();
+    }
+    return someArray;
+}
+Console.Write("[" + String.Join(", ", array) + "]");
