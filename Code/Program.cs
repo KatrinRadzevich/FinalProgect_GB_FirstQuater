1d70a764c87f801e96b7d49cd1b46fc671133aac
Karina Radzevich <karina_radzevich@bk.ru>
2023-04-03 01:46:08 +0400

    array is devided into 4 parts

--- Code/Program.cs
+++ Code/Program.cs
@@ -2,6 +2,19 @@
 string[] array = new string[lenght];
 Console.WriteLine("Введите 4 элемента первоначального массива.Каждый элемент вводится через ENTER: ");
 array = FillArray(array);
+// Делим массив от пользователя на 4 разных массива
+string[] leftPartOrArray = GetLeftPartOfArray(array);
+string[] rightPartOrArray = GetRightPartOfArray(array);
+string[] firstElement = GetLeftPartOfArray(leftPartOrArray);
+string[] secondElement = GetRightPartOfArray(leftPartOrArray);
+string[] thirdElement = GetLeftPartOfArray(rightPartOrArray);
+string[] fourthElement = GetRightPartOfArray(rightPartOrArray);
+
+Console.WriteLine("[" + String.Join(", ", array) + "]");
+Console.WriteLine("[" + String.Join(", ", firstElement) + "]");
+Console.WriteLine("[" + String.Join(", ", secondElement) + "]");
+Console.WriteLine("[" + String.Join(", ", thirdElement) + "]");
+Console.WriteLine("[" + String.Join(", ", fourthElement) + "]");
 // Метод заполнения массива от пользователя
 string[] FillArray(string[] someArray)
 {
@@ -11,4 +24,17 @@ string[] FillArray(string[] someArray)
     }
     return someArray;
 }
-Console.Write("[" + String.Join(", ", array) + "]");
+// Взятие левой части массива как отдельного нового массива
+string[] GetLeftPartOfArray(string[] inArray)
+{
+    int mid = (inArray.Length + 1) / 2;
+    string[] someArray = inArray[..mid];
+    return someArray;
+}
+// Взятие правой части массива как отдельного нового массива
+string[] GetRightPartOfArray(string[] inArray)
+{
+    int mid = (inArray.Length + 1) / 2;
+    string[] someArray = inArray[mid..];
+    return someArray;
+}
